7cd2467cb972c1cd5347328a5b34c7bb39bf7256
tomas.klima <mrklimatomas@gmail.com>
2017-07-14 13:03:26 +0200

    App now correctly shows data from SpringAPI. Added template for data-adding form

--- src/App.js
+++ src/App.js
@@ -22,20 +22,19 @@ class App extends Component {
             });
     };
 
-
     render() {
+        let rows = [];
         if (this.state.data) {
+            for (let i = 0; i < this.state.data._embedded.employees.length; i++) {
+                rows.push(this.renderRow)
+            }
             return (
-                <tbody>
-                <div className="header">
-                    <tr>
-                        <td>FIRST NAME</td>
-                        <td>LAST NAME</td>
-                        <td>COJAVIM</td>
-                    </tr>
+                <div className="ApiApplication">
+                    <Table
+                        data={this.state.data}
+                    />
+                    <Form/>
                 </div>
-                <Rows/>
-                </tbody>
             )
         } else {
 
@@ -44,48 +43,65 @@ class App extends Component {
             )
         }
     }
+
+
 }
-function Rows() {
-    let room = [];
-
-    let data = {
-            "employees": [
-                {
-                    "firstName": "Lukas",
-                    "secondName": "Prijmeni",
-                    "treti": "the third"
-                },
-                {
-                    "firstName": "Jine",
-                    "secondName": "Druhe jmeno",
-                    "treti": "the third"
-
-                },
-                {
-                    "firstName": "John",
-                    "secondName": "Doe",
-                    "treti": "the third"
-
-                }
-            ]
-        }
-    ;
-    let x = data.employees
-    for (let i = 0; i < data.employees.length; i++) {
-        room.push(
-            <tr>
-                <td>{x[i].firstName}</td>
-                <td>{x[i].secondName}</td>
-                <td>{x[i].treti}</td>
-            </tr>)
+class Table extends Component {
+    static renderHeader() {
+        return (
+            <tr className="Header">
+                <td>Name</td>
+                <td>Surname</td>
+                <td>Description</td>
+            </tr>
+        );
 
     }
-    console.log(room);
 
-    return (
-        <div className="data">{room}</div>
-    )
+    renderRow() {
+        console.log(this.props.data._embedded.employees[0].firstName);
+        let data = this.props.data._embedded;
+        let dataRows = [];
+
+        for (let i = 0; i < data.employees.length; i++) {
+            dataRows.push(
+                <tr className="DataRow" id={i}>
+                    <td>{data.employees[i].firstName}</td>
+                    <td>{data.employees[i].lastName}</td>
+                    <td>{data.employees[i].description}</td>
+                </tr>
+            )
+        }
+        return (dataRows)
+    }
+
+    render() {
+        let header = Table.renderHeader();
+        let data = this.renderRow();
+        return (
+            <tbody className="table">
+            {header}
+            {data}
+            </tbody>
+        )
+    }
+
 }
+class Form extends Component {
+    render() {
+        return (
+            <div>
+                <form type="submit">
+                    <input type ="text" />
+                    <input type ="text" />
+                    <input type ="text" />
+                    <button>Hi Ho</button>
+                </form>
+            </div>
+        )
+    }
 
+
+}
 
 export default App;
